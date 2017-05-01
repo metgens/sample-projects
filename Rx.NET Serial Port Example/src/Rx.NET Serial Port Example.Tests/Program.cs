c226f47d0606a21b56d273d2e53a4207d9107777
Etgens <metgens@abis.krakow.pl>
2017-05-01 22:01:12 +0200

    Tests for Rx.NET

--- Rx.NET Serial Port Example/src/Rx.NET Serial Port Example.Tests/Program.cs
+++ Rx.NET Serial Port Example/src/Rx.NET Serial Port Example.Tests/Program.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using FluentAssertions;
-using NUnit.Framework;
-
-namespace Rx.NET_Serial_Port_Example.Tests
+﻿namespace Rx.NET_Serial_Port_Example.Tests
 {
     public class Program
     {
---
+++ Rx.NET Serial Port Example/src/Rx.NET Serial Port Example.Tests/SerialPortDeviceTests/SerialPortDeviceTests.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Microsoft.Reactive.Testing;
+using Moq;
+using NUnit.Framework;
+
+namespace Rx.NET_Serial_Port_Example.Tests.SerialPortDeviceTests
+{
+    [TestFixture]
+    public class SerialPortDeviceTests
+    {
+
+        public class ObserveDataReceived : SerialPortDeviceTests
+        {
+            private Mock<IDataParser> _dataParserMock;
+            private Mock<ISerialPort> _serialPortMock;
+
+            [SetUp]
+            public void Setup()
+            {
+
+                _serialPortMock = new Mock<ISerialPort>();
+                _dataParserMock = new Mock<IDataParser>();
+            }
+
+
+            [Test]
+            public void For2InputEvents_ShouldExecuteDataParserTwice()
+            {
+                var testScheduler = new TestScheduler();
+
+                //arrange
+                var target = new SerialPortDevice(_serialPortMock.Object, _dataParserMock.Object, testScheduler);
+
+                //act
+
+                target.ObserveDataReceived(10d);
+
+                //input events
+                _serialPortMock.Raise(port => port.DataReceived += null, "Test message 1");
+                _serialPortMock.Raise(port => port.DataReceived += null, "Test message 2");
+
+                //assert
+                _dataParserMock.Verify(x => x.Parse(It.Is<string>(y => y == "Test message 1")),Times.Once);
+                _dataParserMock.Verify(x => x.Parse(It.Is<string>(y => y == "Test message 2")),Times.Once);
+
+            }
+
+            [Test]
+            public void ForNoInputFor20sec_ShouldThrowTimeoutException()
+            {
+                var testScheduler = new TestScheduler();
+                //arrange
+                var target = new SerialPortDevice(_serialPortMock.Object, _dataParserMock.Object, testScheduler);
+
+                //act && assert
+                try
+                {
+                    target.ObserveDataReceived(10d);
+
+                    //input event
+                    _serialPortMock.Raise(port => port.DataReceived += null, "Test message");
+                    //wait 20 sec. - more than timeout limit
+                    testScheduler.AdvanceBy(TimeSpan.FromSeconds(20).Ticks);
+                    Assert.Fail("No exception was thrown");
+
+                }
+                catch (Exception ex)
+                {
+                    Assert.IsTrue(ex is TimeoutException);
+                }
+
+
+            }
+
+
+            [Test]
+            public void ForNoInputFor5sec_ShouldNotThrowTimeoutException()
+            {
+                var testScheduler = new TestScheduler();
+                //arrange
+                var target = new SerialPortDevice(_serialPortMock.Object, _dataParserMock.Object, testScheduler);
+
+                //act && assert
+                try
+                {
+                    target.ObserveDataReceived(10d);
+
+                    //input event
+                    _serialPortMock.Raise(port => port.DataReceived += null, "Test message");
+                    //wait 5 sec - less than timeout limit
+                    testScheduler.AdvanceBy(TimeSpan.FromSeconds(5).Ticks);
+
+                }
+                catch (Exception ex)
+                {
+                    Assert.Fail("Exception was thrown but it shouldn't");
+
+                }
+
+
+            }
+        }
+    }
+}
