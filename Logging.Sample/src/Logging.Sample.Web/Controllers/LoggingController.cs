b69732f4860015f8bbdffc8a7aa72aae270d7817
Etgens <metgens@abis.krakow.pl>
2017-05-08 07:32:25 +0200

    Logging works in api controller

--- Logging.Sample/src/Logging.Sample.Web/Controllers/LoggingController.cs
+++ Logging.Sample/src/Logging.Sample.Web/Controllers/LoggingController.cs
@@ -1,16 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace Logging.Sample.Web.Controllers
 {
     [Route("api/[controller]")]
     public class LoggingController : Controller
     {
+        private readonly ILogger<LoggingController> _logger;
+
+        public LoggingController(ILogger<LoggingController> logger)
+        {
+            _logger = logger;
+        }
+
         // GET api/values
         [HttpGet]
         public IActionResult Get()
         {
             var testMessage = "Logging example test";
-
+            _logger.LogInformation(testMessage);
 
             return Ok(testMessage);
         }
